bf40c9ec98493a065d7554ea075d2bdcf61a4c41
Mateusz <mateusz.kozyra56@gmail.com>
2019-01-09 23:48:17 +0100

    Update to3bit.cpp
    Reorde()

--- src/to3bit.cpp
+++ src/to3bit.cpp
@@ -1,165 +1,229 @@
 #ifdef __linux__
-    #include "include/algorytmy.h"
+#include "include/algorytmy.h"
 #else
-    #include "algorytmy.h"
+#include "algorytmy.h"
 #endif
 
 #include <math.h>
 
 using namespace std;
 
+int FindIndex(SDL_Color pixel, Pallete paleta);
+Pallete NarzuconaPaleta();
+Pallete SkalaSzarosci();
+Pallete SkalaSzarosci(Pallete paleta);
+SDL_Color FindColor(SDL_Color pixel, Pallete paleta);
+int FindIndex(SDL_Color pixel, Pallete paleta);
+void to3bitBMP(Bitmap &in, Bitmap &out, Pallete paleta);
+Bit3BMP to3bitTab(Bitmap &in, Pallete paleta);
+void test4(Screen & screen);
+
+Pallete Reorder(Pallete paleta, Bitmap &in)
+{
+	int tab[8] = {};
+	int resX = in.getWidth();
+	int resY = in.getHeight();
+
+	for (int i = 0; i < resX; ++i)
+		for (int j = 0; j < resY; ++j)
+		{
+			tab[FindIndex(in.getPixel(i, j), paleta)]++;
+		}
+
+	Pallete ret;
+	for (int j = 0; j < 8; j++)
+	{
+		int max = -1;
+		int maxIndex;
+		for (int i = 0; i < 8; i++)
+		{
+			if (tab[i] > max)
+			{
+				max = tab[i];
+				maxIndex = i;
+			}
+		}
+		tab[maxIndex] = 0;
+		ret.c[j] = paleta.c[maxIndex];
+	}
+	return ret;
+}
+
 Pallete NarzuconaPaleta()
 {
-Pallete narzuconaPaleta;
-narzuconaPaleta.c[0].r = 255;
-narzuconaPaleta.c[0].g = 255;
-narzuconaPaleta.c[0].b = 255;
+	Pallete narzuconaPaleta;
+	narzuconaPaleta.c[0].r = 255;
+	narzuconaPaleta.c[0].g = 255;
+	narzuconaPaleta.c[0].b = 255;
 
-narzuconaPaleta.c[1].r = 0;
-narzuconaPaleta.c[1].g = 0;
-narzuconaPaleta.c[1].b = 0;
+	narzuconaPaleta.c[1].r = 0;
+	narzuconaPaleta.c[1].g = 0;
+	narzuconaPaleta.c[1].b = 0;
 
-narzuconaPaleta.c[2].r = 255;
-narzuconaPaleta.c[2].g = 0;
-narzuconaPaleta.c[2].b = 0;
+	narzuconaPaleta.c[2].r = 255;
+	narzuconaPaleta.c[2].g = 0;
+	narzuconaPaleta.c[2].b = 0;
 
-narzuconaPaleta.c[3].r = 0;
-narzuconaPaleta.c[3].g = 255;
-narzuconaPaleta.c[3].b = 0;
+	narzuconaPaleta.c[3].r = 0;
+	narzuconaPaleta.c[3].g = 255;
+	narzuconaPaleta.c[3].b = 0;
 
-narzuconaPaleta.c[4].r = 0;
-narzuconaPaleta.c[4].g = 0;
-narzuconaPaleta.c[4].b = 255;
+	narzuconaPaleta.c[4].r = 0;
+	narzuconaPaleta.c[4].g = 0;
+	narzuconaPaleta.c[4].b = 255;
 
-narzuconaPaleta.c[5].r = 255;
-narzuconaPaleta.c[5].g = 255;
-narzuconaPaleta.c[5].b = 0;
+	narzuconaPaleta.c[5].r = 255;
+	narzuconaPaleta.c[5].g = 255;
+	narzuconaPaleta.c[5].b = 0;
 
-narzuconaPaleta.c[6].r = 0;
-narzuconaPaleta.c[6].g = 255;
-narzuconaPaleta.c[6].b = 255;
+	narzuconaPaleta.c[6].r = 0;
+	narzuconaPaleta.c[6].g = 255;
+	narzuconaPaleta.c[6].b = 255;
 
-narzuconaPaleta.c[7].r = 255;
-narzuconaPaleta.c[7].g = 0;
-narzuconaPaleta.c[7].b = 255;
-return narzuconaPaleta;
+	narzuconaPaleta.c[7].r = 255;
+	narzuconaPaleta.c[7].g = 0;
+	narzuconaPaleta.c[7].b = 255;
+	return narzuconaPaleta;
 }
 
 Pallete SkalaSzarosci()
 {
-    Pallete paleta;
-paleta.c[0].r = 0;
-paleta.c[0].g = 0;
-paleta.c[0].b = 0;
+	Pallete paleta;
+	paleta.c[0].r = 0;
+	paleta.c[0].g = 0;
+	paleta.c[0].b = 0;
 
-paleta.c[1].r = 36;
-paleta.c[1].g = 36;
-paleta.c[1].b = 36;
+	paleta.c[1].r = 36;
+	paleta.c[1].g = 36;
+	paleta.c[1].b = 36;
 
-paleta.c[2].r = 72;
-paleta.c[2].g = 72;
-paleta.c[2].b = 72;
+	paleta.c[2].r = 72;
+	paleta.c[2].g = 72;
+	paleta.c[2].b = 72;
 
-paleta.c[3].r = 108;
-paleta.c[3].g = 108;
-paleta.c[3].b = 108;
+	paleta.c[3].r = 108;
+	paleta.c[3].g = 108;
+	paleta.c[3].b = 108;
 
-paleta.c[4].r = 144;
-paleta.c[4].g = 144;
-paleta.c[4].b = 144;
+	paleta.c[4].r = 144;
+	paleta.c[4].g = 144;
+	paleta.c[4].b = 144;
 
-paleta.c[5].r = 180;
-paleta.c[5].g = 180;
-paleta.c[5].b = 180;
+	paleta.c[5].r = 180;
+	paleta.c[5].g = 180;
+	paleta.c[5].b = 180;
 
-paleta.c[6].r = 216;
-paleta.c[6].g = 216;
-paleta.c[6].b = 216;
+	paleta.c[6].r = 216;
+	paleta.c[6].g = 216;
+	paleta.c[6].b = 216;
 
-paleta.c[7].r = 255;
-paleta.c[7].g = 255;
-paleta.c[7].b = 255;
-return paleta;
+	paleta.c[7].r = 255;
+	paleta.c[7].g = 255;
+	paleta.c[7].b = 255;
+	return paleta;
+}
+
+Pallete SkalaSzarosci(Pallete paleta)
+{
+	Pallete ret;
+	for (int i = 0; i < 8; i++)
+	{
+		int BW = paleta.c[i].r*0.299 + paleta.c[i].g*0.587 + paleta.c[i].b*0.114;
+		ret.c[i].r = BW;
+		ret.c[i].g = BW;
+		ret.c[i].b = BW;
+	}
+	return ret;
 }
 
 SDL_Color FindColor(SDL_Color pixel, Pallete paleta)
 {
-    int minDistance = 1000;
-    int minIndeks = 0;
-    for(int i = 0; i < 8; ++i)
-    {
-        int distance = sqrt(pow(pixel.r - paleta.c[i].r, 2) + pow(pixel.g - paleta.c[i].g, 2) + pow(pixel.b - paleta.c[i].b, 2));
-        if (distance < minDistance)
-        {
-            minDistance = distance;
-            minIndeks = i;
-        }
-    }
-    return paleta.c[minIndeks];
+	int minDistance = 1000;
+	int minIndeks = 0;
+	for (int i = 0; i < 8; ++i)
+	{
+		int distance = sqrt(pow(pixel.r - paleta.c[i].r, 2) + pow(pixel.g - paleta.c[i].g, 2) + pow(pixel.b - paleta.c[i].b, 2));
+		if (distance < minDistance)
+		{
+			minDistance = distance;
+			minIndeks = i;
+		}
+	}
+	return paleta.c[minIndeks];
 }
 
 int FindIndex(SDL_Color pixel, Pallete paleta)
 {
-    int minDistance = 1000;
-    int minIndeks = 0;
-    for(int i = 0; i < 8; ++i)
-    {
-        int distance = sqrt(pow(pixel.r - paleta.c[i].r, 2) + pow(pixel.g - paleta.c[i].g, 2) + pow(pixel.b - paleta.c[i].b, 2));
-        if (distance < minDistance)
-        {
-            minDistance = distance;
-            minIndeks = i;
-        }
-    }
-    return minIndeks;
+	int minDistance = 1000;
+	int minIndeks = 0;
+	for (int i = 0; i < 8; ++i)
+	{
+		int distance = sqrt(pow(pixel.r - paleta.c[i].r, 2) + pow(pixel.g - paleta.c[i].g, 2) + pow(pixel.b - paleta.c[i].b, 2));
+		if (distance < minDistance)
+		{
+			minDistance = distance;
+			minIndeks = i;
+		}
+	}
+	return minIndeks;
 }
 
 void to3bitBMP(Bitmap &in, Bitmap &out, Pallete paleta)
 {
-    int resX = in.getWidth();
-    int resY = in.getHeight();
+	int resX = in.getWidth();
+	int resY = in.getHeight();
 
-    for(int i = 0; i < resX; ++i)
-        for(int j = 0; j < resY; ++j)
-        {
-            out.setPixel(i,j, FindColor(in.getPixel(i,j), paleta));
-        }
+	for (int i = 0; i < resX; ++i)
+		for (int j = 0; j < resY; ++j)
+		{
+			out.setPixel(i, j, FindColor(in.getPixel(i, j), paleta));
+		}
 }
 
 Bit3BMP to3bitTab(Bitmap &in, Pallete paleta)
 {
-    Bit3BMP ret;
-    int resX = in.getWidth();
-    int resY = in.getHeight();
-    ret.data.resize(resX * resY);
+	Bit3BMP ret;
+	int resX = in.getWidth();
+	int resY = in.getHeight();
+	ret.data.resize(resX * resY);
 
-    int indeks = 0;
-    for(int i = 0; i < resX; ++i)
-        for(int j = 0; j < resY; ++j)
-        {
-            ret.data[indeks++] = FindIndex(in.getPixel(j,i), paleta);
-        }
-    return ret;
+	int indeks = 0;
+	for (int i = 0; i < resX; ++i)
+		for (int j = 0; j < resY; ++j)
+		{
+			ret.data[indeks++] = FindIndex(in.getPixel(j, i), paleta);
+		}
+	return ret;
 }
 
 void test4(Screen & screen)
 {
-    Bitmap bmp("obrazek4.bmp");
-    Bitmap bmp1("obrazek4.bmp");
-    Bitmap bmp2("obrazek4.bmp");
-    Bitmap bmp3("obrazek4.bmp");
-    screen.draw(bmp,0,0);
-    Pallete paleta;
+	Bitmap bmp("obrazek2.bmp");
+	Bitmap bmp1("obrazek2.bmp");
+	Bitmap bmp2("obrazek2.bmp");
+	Bitmap bmp3("obrazek2.bmp");
+	Bitmap bmp4("obrazek2.bmp");
 
-    paleta = getPallete(bmp);
-    to3bitBMP(bmp, bmp1, paleta);
-    screen.draw(bmp1, bmp1.getWidth(), 0);
+	screen.draw(bmp, 0, 0);
+	Pallete paleta;
 
-    paleta = NarzuconaPaleta();
-    to3bitBMP(bmp, bmp2,paleta);
-    screen.draw(bmp2, 0, bmp2.getHeight());
+	paleta = getPallete(bmp);
+	paleta = Reorder(paleta, bmp);
+	to3bitBMP(bmp, bmp1, paleta);
+	screen.draw(bmp1, bmp1.getWidth(), 0);
 
-    paleta = SkalaSzarosci();
-    to3bitBMP(bmp, bmp3, paleta);
-    screen.draw(bmp3, bmp3.getWidth(), bmp3.getHeight());
+	paleta = SkalaSzarosci(paleta);
+	paleta = Reorder(paleta, bmp);
+	to3bitBMP(bmp, bmp4, paleta);
+	screen.draw(bmp4, 0, 0);
+
+	paleta = NarzuconaPaleta();
+	paleta = Reorder(paleta, bmp);
+	to3bitBMP(bmp, bmp2, paleta);
+	screen.draw(bmp2, 0, bmp2.getHeight());
+
+	paleta = SkalaSzarosci();
+	paleta = Reorder(paleta, bmp);
+	to3bitBMP(bmp, bmp3, paleta);
+	screen.draw(bmp3, bmp3.getWidth(), bmp3.getHeight());
 }
